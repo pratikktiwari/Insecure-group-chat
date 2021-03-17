9d6437f3286ce49fd1ad005303954fdbae0ca102
pratikktiwari <ipratiktiwari@gmail.com>
2021-03-17 09:48:28 +0530

    get User IP

--- chatInterface.php
+++ chatInterface.php
@@ -10,6 +10,7 @@
     <link rel="stylesheet" href="css/chat.css" />
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
 </head>
 
 <body>
---
+++ getIP.php
@@ -0,0 +1,5 @@
+<?php
+    $json = file_get_contents("http://ip-api.com/php/");
+    // print_r($json);
+    echo $json;
+?>
